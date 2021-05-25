ddd799117e6600c90e37a8259fd5f61e07a5195b
srinivasmyk <65235119+srinivasmyk@users.noreply.github.com>
2021-05-25 18:37:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,10 +7,10 @@
 // DIFFERENT DATA! Contains movement dates, currency and locale
 
 const account1 = {
-  owner: 'Jonas Schmedtmann',
+  owner: 'Srinivas Myakala',
   movements: [200, 455.23, -306.5, 25000, -642.21, -133.9, 79.97, 1300],
   interestRate: 1.2, // %
-  pin: 1111,
+  pin: 1234,
 
   movementsDates: [
     '2019-11-18T21:31:17.178Z',
